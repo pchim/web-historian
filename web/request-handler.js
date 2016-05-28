4dc865c1e8242ceba3f3355238098c9e200593d9
Peter Chim <pchim24@gmail.com>
2016-05-27 22:26:02 -0700

    Serve up index.html on GET /

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,35 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
 // require more modules/folders here!
+var fs = require('fs');
 
-exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+const reqOptions = (req, res, method) => {
+
+  // act based on method
+  const reqObj = {
+    'GET': () => {
+      console.log('finding route');
+      if (req.url === '/') {
+        fs.readFile(archive.paths.siteAssets + '/index.html', 'utf8', (err, data) => {
+          console.log('reading data');
+          res.write(data);
+          res.statusCode = 200;
+          res.end(data);
+        });
+      }
+    }
+  };
+
+  console.log('entering reqoptions');
+  reqObj[method]();
+
+};
+
+exports.handleRequest = (req, res) => {
+
+  console.log('handling request ', req.method);
+  reqOptions(req, res, req.method);
+
+
+  //res.end(archive.paths.list);
 };
