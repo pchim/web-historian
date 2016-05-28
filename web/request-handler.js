ea1016d6643dc1da59a83dd2a6212479719673ef
Peter Chim <pchim24@gmail.com>
2016-05-27 22:37:58 -0700

    Return contents on GET of archived site files or 404 if non-existent

--- web/request-handler.js
+++ web/request-handler.js
@@ -8,15 +8,28 @@ const reqOptions = (req, res, method) => {
   // act based on method
   const reqObj = {
     'GET': () => {
-      console.log('finding route');
+      console.log('finding route, url: ', req.url);
+      
+      // find path according to url
+      var filePath;
       if (req.url === '/') {
-        fs.readFile(archive.paths.siteAssets + '/index.html', 'utf8', (err, data) => {
-          console.log('reading data');
+        filePath = archive.paths.siteAssets + '/index.html';
+      } else {
+        filePath = archive.paths.archivedSites + req.url;
+      }
+
+      // read data in from file
+      fs.readFile(filePath, 'utf8', (err, data) => {
+        console.log('reading data');
+        if (err) {
+          res.statusCode = 404;
+          res.end();
+        } else {
           res.write(data);
           res.statusCode = 200;
           res.end(data);
-        });
-      }
+        }
+      });
     }
   };
 
@@ -29,7 +42,6 @@ exports.handleRequest = (req, res) => {
 
   console.log('handling request ', req.method);
   reqOptions(req, res, req.method);
-
 
   //res.end(archive.paths.list);
 };
